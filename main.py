ace20ff018fa6d38e1cbbe2946d5c78d4cb37f4e
Cookie Doodle <kristina1stawizki@gmail.com>
2022-12-14 15:47:11 +0100

    Karl(34)

--- main.py
+++ main.py
@@ -32,3 +32,77 @@ if bool(numi):
     print("YES")
 else:
     print("NO")
+
+gay = True
+if gay:
+    print("You are gay")
+
+
+class MathDo:
+    x = 5
+
+
+p1 = MathDo()
+print(p1.x)
+
+"""
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+"""
+
+"""
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+
+p1 = Person("John", 36)
+
+print(p1)
+"""
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+# Karl(34) Input from User for TextAdventure!
+class UserInput:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello! Your name is " + self.name + ".")
+        print("You are " + self.age + " years old.")
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+
+print("Welcome to this Text Adventure!")
+user1 = UserInput(input("But first! Who are you?\n"), input("And how old are you?\n"))
+user1.myfunc()
+
+print(user1)
